3eea4392f440cbd646698a41d944f227e58ca051
AnjaliSapkota <sapkotaanjali123@gmail.com>
2026-07-12 17:18:10 +0545

    ft: data preprocess and analyze label distributions

--- load.py
+++ load.py
@@ -2,14 +2,5 @@ from datasets import load_dataset
 
 dataset = load_dataset("imdb")
 
-print(dataset)
-
-print(dataset.keys())                  
-print(dataset["train"].features)       
-print(len(dataset["train"]))           
-print(dataset["train"][0])            
-print(dataset["train"][0]["text"])     
-print(dataset["train"][0]["label"])   
-
-
-
+train = dataset["train"]
+test = dataset["test"]
